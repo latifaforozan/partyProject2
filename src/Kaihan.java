fed8a2dc360946ab6e2b96004df2030265f831f4
latifaforozan <la.forozan@gmail.com>
2022-12-26 12:21:17 -0800

    updated kaihan file

--- src/Kaihan.java
+++ src/Kaihan.java
@@ -3,6 +3,7 @@ public class Kaihan {
     public static void main(String[] args) {
 
         System.out.println("Creating out file to see it for practicing ");
+        System.out.println("We are the best group for ever");
 
     }
 }
